7da91ddfb624bbf704e9e18bff64c64b16f6bc2d
Sam <elucidation@gmail.com>
2018-04-09 00:14:36 -0700

    Add X-corner ransac based chessboard detection notebook

--- RunExportedMLOnImage.py
+++ RunExportedMLOnImage.py
@@ -104,7 +104,7 @@ def pruneSaddle(s, init=128):
 
 
 def loadImage(filepath):
-    img_orig = PIL.Image.open(filepath)
+    img_orig = PIL.Image.open(filepath).convert('RGB')
     img_width, img_height = img_orig.size
 
     # Resize
@@ -142,6 +142,47 @@ def input_fn_predict(img_data): # returns x, None
     dataset = dataset.batch(25)
     return dataset.make_one_shot_iterator().get_next(), None
   return ret_func
+
+def removeOutlierSimplices(tri):
+    dists = np.zeros([tri.nsimplex, 3])
+    for i,triangle in enumerate(tri.points[tri.simplices]):
+        # We want the distance of the edge opposite the vertex k, so r_k.
+        r0 = (triangle[2,:] - triangle[1,:])
+        r1 = (triangle[0,:] - triangle[2,:])
+        r2 = (triangle[1,:] - triangle[0,:])
+        dists[i,:] = np.linalg.norm(np.vstack([r0,r1,r2]), axis=1)
+    # Threshold based on twice the smallest edge.
+    threshold = 2*np.median(dists)
+    # Find edges that are too long
+    long_edges = dists > threshold
+    long_edged_simplices = np.any(long_edges,axis=1)
+    # Good simplices are those that don't contain any long edges
+    good_simplices_mask = ~long_edged_simplices
+#     good_simplices = tri.simplices[good_simplices_mask]
+    return dists, good_simplices_mask
+
+def findQuadSimplices(tri, dists, simplices_mask=None):
+    vertex_idx_opposite_longest_edge = dists.argmax(axis=1)
+    # The neighboring tri for each tri about the longest edge
+    potential_neighbor = tri.neighbors[
+        np.arange(tri.nsimplex),
+        vertex_idx_opposite_longest_edge]
+    good_neighbors = []
+    for i,j in enumerate(potential_neighbor):
+        if j == -1:
+            # Skip those that don't have a neighbor
+            continue
+        # If these tris both agree that they're good neighbors, keep them.
+        if (potential_neighbor[j] == i and i < j):
+#             print(i,j)
+            if simplices_mask is not None:
+                if simplices_mask[i]:
+                    good_neighbors.append(i)
+                if simplices_mask[j]:
+                    good_neighbors.append(j)
+            else:
+                good_neighbors.extend([i,j])
+    return good_neighbors
 
 def videostream(filename='carlsen_match.mp4', SAVE_FRAME=True):
   # vidstream = skvideo.io.vread('VID_20170427_003836.mp4')
@@ -183,7 +224,10 @@ def videostream(filename='carlsen_match.mp4', SAVE_FRAME=True):
     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    inlier_pts, outlier_pts, pred_pts, final_predictions, prediction_levels, tri = processImage(gray)
+    # if i==900:
+    #   cv2.imwrite("frame%d.png" % i, frame)
+
+    inlier_pts, outlier_pts, pred_pts, final_predictions, prediction_levels, tri, simplices_mask = processImage(gray)
 
 
     for pt in inlier_pts:
@@ -197,22 +241,9 @@ def videostream(filename='carlsen_match.mp4', SAVE_FRAME=True):
       cv2.polylines(frame,
         np.flip(inlier_pts[tri.simplices].astype(np.int32), axis=2),
         isClosed=True, color=(255,0,0))
-
-    # for c,pt,pct in zip(final_predictions, pred_pts, prediction_levels):
-    #   # color_level = int(50 + 205 * pct)
-    #   # radius_size = int(1 + 3*pct)
-    #   if (c == 1):
-    #     if pct > 0.8:
-    #       # Good.
-    #       cv2.circle(frame, tuple(pt[::-1]), 3, (0,255,0), -1)
-    #     # elif pct > 0.8:
-    #     #   # Okay.
-    #     #   cv2.circle(frame, tuple(pt[::-1]), 2, (0,100,0), -1)
-    #   #   else:
-    #   #     # Pretty bad but not a complete failure.
-    #   #     cv2.circle(frame, tuple(pt[::-1]), 1, (0,100,100), -1)
-    #   # else:
-    #   #   cv2.circle(frame, tuple(pt[::-1]), 1, (0,0,0), -1)
+      cv2.polylines(frame,
+        np.flip(inlier_pts[tri.simplices[simplices_mask]].astype(np.int32), axis=2),
+        isClosed=True, color=(0,255,0), thickness=3)
 
     cv2.putText(frame, 'Frame %d' % i, (5,15), cv2.FONT_HERSHEY_PLAIN, 1.0,(255,255,255),0,cv2.LINE_AA)
 
@@ -223,6 +254,7 @@ def videostream(filename='carlsen_match.mp4', SAVE_FRAME=True):
       cv2.imwrite(output_filepath, frame)
 
     # if i==900:
+    #   cv2.imwrite("frame%d_overlay.png" % i, frame)
     #   print(np.array(pred_pts)[np.array(prediction_levels)>0.8,:])
     #   break;
 
@@ -239,22 +271,12 @@ def calculateOutliers(pts, threshold_mult = 2.5):
   ctr = np.mean(pts, axis=0)
   return (np.any(np.abs(pts-ctr) > threshold_mult * std, axis=1))
 
-# def calculateOutliers(pts, threshold_mult = 3):
-#   N = len(pts)
-#   dists = np.zeros([N,N])
-#   best_dists = np.zeros(N)
-#   for i in range(N):
-#     dists[i,:] = np.linalg.norm(pts[:,:] - pts[i,:], axis=1)
-#     x = np.linalg.norm(pts - pts[i,:], axis=1)
-#     best_dists[i] = np.min(x[x!=0])
-#   med = np.median(best_dists)
-#   return best_dists > med*threshold_mult
-
 def processImage(img_gray):
-  a = time.time()
+  print("Processing.")
+  a_start = time.time()
   spts = getFinalSaddlePoints(img_gray)
   b = time.time()
-  t_saddle = (b-a)
+  t_saddle = (b-a_start)
   WINSIZE = 5
 
   tiles = []
@@ -267,6 +289,10 @@ def processImage(img_gray):
       tile = img_gray[pt[0]-WINSIZE:pt[0]+WINSIZE+1, pt[1]-WINSIZE:pt[1]+WINSIZE+1]
       tiles.append(tile)
       pred_pts.append(pt)
+
+  if tiles == []:
+    return [None]*7
+
   tiles = np.array(tiles, dtype=np.uint8)
 
   a = time.time()
@@ -288,7 +314,6 @@ def processImage(img_gray):
     prediction_levels.append(prediction[1])
   b = time.time()
   t_pred = b-a
-  print(" - Saddle took %.2f ms (%d pts), Predict took %.2f ms" % (t_saddle*1e3, len(spts), t_pred*1e3)) # ~2-3ms
 
   ml_pts = np.array(pred_pts)[np.array(prediction_levels)>0.8,:]
   bad_pts_mask = calculateOutliers(ml_pts)
@@ -296,33 +321,46 @@ def processImage(img_gray):
   inlier_pts = ml_pts[~bad_pts_mask,:]
   outlier_pts = ml_pts[bad_pts_mask,:]
 
-  if (len(inlier_pts) >= 3):
+  if (len(inlier_pts) >= 4):
     tri = Delaunay(inlier_pts)
+    dists, good_simplices_mask = removeOutlierSimplices(tri)
+    good_neighbors = findQuadSimplices(tri, dists, good_simplices_mask)
+    simplices_mask = good_simplices_mask
+    # simplices_mask = np.zeros(tri.nsimplex, dtype=bool)
+    # simplices_mask[good_neighbors] = True
   else:
     tri = None
+    simplices_mask = None
+
+  b_end = time.time()
+  t_total = b_end-a_start
+  print(" - Total %.2f ms, Saddle took %.2f ms (%d pts), Predict took %.2f ms" % (t_total*1e3, t_saddle*1e3, len(spts), t_pred*1e3)) # ~2-3ms
+
+  return inlier_pts, outlier_pts, pred_pts, final_predictions, prediction_levels, tri, simplices_mask
 
 
-  return inlier_pts, outlier_pts, pred_pts, final_predictions, prediction_levels, tri
 
 
-
-
-def main():
+def main(SAVE_FRAME=False):
   # filenames = glob.glob('input_bad/*')
-  # filenames = glob.glob('input/img_*')
-  # filenames.extend(glob.glob('input_yt/*.jpg'))
-  filenames = (glob.glob('input_yt/*.jpg'))
+  filenames = glob.glob('input/img_*')
+  filenames.extend(glob.glob('input_yt/*.jpg'))
+  # filenames = (glob.glob('input_yt/*.jpg'))
   filenames = sorted(filenames)
   n = len(filenames)
   # n = 5
 
   WINSIZE = 5
 
+  output_folder = 'ml_images'
+  if not os.path.exists(output_folder):
+    os.mkdir(output_folder)
+
   for i in range(n):
     filename = filenames[i]
     print ("Processing %d/%d : %s" % (i+1,n,filename))
     img, img_gray = loadImage(filename)
-    inlier_pts, outlier_pts, pred_pts, final_predictions, prediction_levels, tri = processImage(img_gray)
+    inlier_pts, outlier_pts, pred_pts, final_predictions, prediction_levels, tri, simplices_mask = processImage(img_gray)
 
     b,g,r = cv2.split(img)       # get b,g,r
     rgb_img = cv2.merge([r,g,b])     # switch it to rgb
@@ -337,9 +375,20 @@ def main():
       cv2.polylines(rgb_img,
         np.flip(inlier_pts[tri.simplices].astype(np.int32), axis=2),
         isClosed=True, color=(255,0,0))
+      cv2.polylines(rgb_img,
+        np.flip(inlier_pts[tri.simplices[simplices_mask]].astype(np.int32), axis=2),
+        isClosed=True, color=(0,255,0), thickness=3)
 
     cv2.imshow('frame',rgb_img)
-    if cv2.waitKey(100) & 0xFF == ord('q'):
+
+    output_filepath = '%s/%s_ml.png' % (output_folder,filename[:-3])
+    if SAVE_FRAME:
+      if not os.path.exists(os.path.dirname(output_filepath)):
+        os.makedirs(os.path.dirname(output_filepath))
+      print("Saving %s" % output_filepath)
+      cv2.imwrite(output_filepath, rgb_img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
       break
 
   print('Finished')
@@ -350,13 +399,13 @@ def main():
 
 
 if __name__ == '__main__':
-  # main()
+  # main(True)
   # filename = 'carlsen_match.mp4'
   # filename = 'carlsen_match2.mp4'
   # filename = 'output.avi'
   # filename = 'output2.avi'
-  # filename = 'random1.mp4'
-  filename = 'speedchess1.mp4'
+  filename = 'random1.mp4'
+  # filename = 'speedchess1.mp4'
   # filename = 'match1.mp4'
   # filename = 'match2.mp4'
   videostream(filename, True)
